77a62909332b5da19b77a6860cbedadcef10a490
Vladislav Voscanean <vvosc001@odu.edu>
2023-07-12 19:38:03 -0400

    Start Repository

---
+++ Adventurer.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <random>
+#include "Adventurer.h"
+
+using namespace std;
+
+int main(){
+
+}
---
+++ Adventurer.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <random>
+#include <string>
+
+using namespace std;
+
+class Adventurer{
+    string Name;
+    int Dexterity;
+    int Strength;
+    int Constitution;
+    int Intelligence;
+    int Charisma;
+    int Wisdom;
+    
+}
