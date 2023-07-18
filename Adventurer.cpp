e5d106d8ac123f5a6cda3272f0d5f0c1c8868e71
Vladislav Voscanean <vvosc001@odu.edu>
2023-07-18 01:44:02 -0400

    Add random number generator for attributes.
    Co-authored-by: Vidal Tan <Vidalatan@users.noreply.github.com>

--- Adventurer.cpp
+++ Adventurer.cpp
@@ -4,22 +4,24 @@
 #include <list>
 #include <array>
 #include <iterator>
+#include <random>
+#include <functional>
 
 using namespace std;
 
-class Adventurer{
+class Adventurer {
     public:
-
-        Adventurer (string newname){
+        Adventurer(string newname) {
             Name = newname;
-            Dexterity = 0;
-            Strength = 0;
-            Constitution = 0;
-            Intelligence = 0;
-            Charisma = 0;
-            Wisdom = 0;
+            Dexterity = getRandomNumber(4, 6);
+            Strength = getRandomNumber(4, 6);
+            Constitution = getRandomNumber(4, 6);
+            Intelligence = getRandomNumber(4, 6);
+            Charisma = getRandomNumber(4, 6);
+            Wisdom = getRandomNumber(4, 6);
+            Level = 1;
         }
-        void display(){
+        void display() {
             cout << "Name: " << this->getName() << endl;
             cout << "Dexterity: " << this->getDexterity() << endl;
             cout << "Strength: " << this->getStrength() << endl;
@@ -27,44 +29,46 @@ class Adventurer{
             cout << "Intelligence: " << this->getIntelligence() << endl;
             cout << "Charisma: " << this->getCharisma() << endl;
             cout << "Wisdom: " << this->getWisdom() << endl;
+            cout << "Level " << this->getLevel() << endl;
         }
-        int getDexterity(){
+
+        int getDexterity() {
             return this->Dexterity;
         }
-        void setStrength(int _Strength){
+        void setStrength(int _Strength) {
             this->Strength = _Strength;
         }
-        int getStrength(){
+        int getStrength() {
             return this->Strength;
         }
-        void setConstitution(int _Constitution){
+        void setConstitution(int _Constitution) {
             this->Constitution = _Constitution;
         }
-        int getConstitution(){
+        int getConstitution() {
             return this->Constitution;
         }
-        void setIntelligence(int _Intelligence){
+        void setIntelligence(int _Intelligence) {
             this->Intelligence = _Intelligence;
         }
-        int getIntelligence(){
+        int getIntelligence() {
             return this->Intelligence;
         }
-        void setCharisma(int _Charisma){
+        void setCharisma(int _Charisma) {
             this->Charisma = _Charisma;
         }
-        int getCharisma(){
+        int getCharisma() {
             return this->Charisma;
         }
-        void setWisdom(int _Wisdom){
+        void setWisdom(int _Wisdom) {
             this->Wisdom = _Wisdom;
         }
-        int getWisdom(){
+        int getWisdom() {
             return this->Wisdom;
         }
-        string getName (){
+        string getName() {
             return this->Name;
         }
-        void setName(string _Name){
+        void setName(string _Name) {
             this->Name = _Name;
         }
 
@@ -77,21 +81,27 @@ class Adventurer{
         int Intelligence;
         int Charisma;
         int Wisdom;  
+        int getRandomNumber(int min, int max) {
+            random_device rd;
+            mt19937 gen(rd());
+            uniform_int_distribution<int> dis(min, max);
+            return dis(gen);
+        }
 };
-int main(){
+int main() {
     int numAdvent;
     list<Adventurer> adventurerList; 
     cout << "How many adventurers would you like to create? " << endl;
     cin >> numAdvent;
-    for (int i=0; i<numAdvent; i++){
+    for (int i = 0; i < numAdvent; i++) {
         string adventName;
-        cout << "Enter name for Adventurer " << i+1 << endl;
+        cout << "Enter name for Adventurer " << i + 1 << endl;
         cin >> adventName; 
         Adventurer advent(adventName);
         adventurerList.push_back(advent);
     }
     list<Adventurer>::iterator adventureritr;
-    for (adventureritr=adventurerList.begin(); adventureritr !=adventurerList.end(); adventureritr++){
+    for (adventureritr = adventurerList.begin(); adventureritr != adventurerList.end(); adventureritr++) {
         adventureritr->display();
     }
     return 0;
