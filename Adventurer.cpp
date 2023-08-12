860d13687d712c05b85fdea67d6116606214e127
Vladislav Voscanean <vvosc001@odu.edu>
2023-08-12 04:13:34 -0400

    Move definitions to Adventurer.h

--- Adventurer.cpp
+++ Adventurer.cpp
@@ -7,13 +7,15 @@
 #include <iterator>
 #include <unordered_map>
 #include <functional>
+#include "Adventurer.h"
 
 using namespace std;
 
-class Adventurer {
+class Inventory;
 
-    public:
-        Adventurer(string newname) {
+Adventurer::Adventurer (string newname) {
+
+    Inventory inventory;
             Name = newname;
             Dexterity = getRandomNumber(4, 6);
             Strength = getRandomNumber(4, 6);
@@ -22,104 +24,7 @@ class Adventurer {
             Charisma = getRandomNumber(4, 6);
             Wisdom = getRandomNumber(4, 6);
             Level = 1;
-        }
-        void levelUp() {
-            this->Level=this->Level+1;
-        }
-                    
-        void display() {
-            cout << "Name: " << this->getName() << endl;
-            cout << "Dexterity: " << this->getDexterity() << endl;
-            cout << "Strength: " << this->getStrength() << endl;
-            cout << "Constitution: " << this->getConstitution() << endl;
-            cout << "Intelligence: " << this->getIntelligence() << endl;
-            cout << "Charisma: " << this->getCharisma() << endl;
-            cout << "Wisdom: " << this->getWisdom() << endl;
-            cout << "Level " << this->getLevel() << endl;
-        }
-
-        int getDexterity() {
-            return this->Dexterity;
-        }
-        void setStrength(int _Strength) {
-            this->Strength = _Strength;
-        }
-        int getStrength() {
-            return this->Strength;
-        }
-        void setConstitution(int _Constitution) {
-            this->Constitution = _Constitution;
-        }
-        int getConstitution() {
-            return this->Constitution;
-        }
-        void setIntelligence(int _Intelligence) {
-            this->Intelligence = _Intelligence;
-        }
-        int getIntelligence() {
-            return this->Intelligence;
-        }
-        void setCharisma(int _Charisma) {
-            this->Charisma = _Charisma;
-        }
-        int getCharisma() {
-            return this->Charisma;
-        }
-        void setWisdom(int _Wisdom) {
-            this->Wisdom = _Wisdom;
-        }
         
-        void addToWisdom(int points) {
-            this->Wisdom += points;
-        }
-
-        void addToStrength(int points) {
-            this->Strength += points;
-        }
-
-        void addToIntelligence(int points) {
-            this->Intelligence += points;
-        }
-
-        void addToDexterity(int points) {
-            this->Dexterity += points;
-        }
-
-        void addToConstitution(int points) {
-            this->Constitution += points;
-        }
-
-        void addToCharisma(int points) {
-            this->Charisma += points;
-        }
-
-        int getWisdom() {
-            return this->Wisdom;
-        }
-        string getName() {
-            return this->Name;
-        }
-        void setName(string _Name) {
-            this->Name = _Name;
-        }
-        int getLevel() {
-            return this->Level;
-        }
-        int getRandomNumber(int min, int max) {
-            random_device rd;
-            mt19937 gen(rd());
-            uniform_int_distribution<int> dis(min, max);
-            return dis(gen);
-        }
-    private:
-        string Name;
-        int Dexterity;
-        int Strength;
-        int Constitution;
-        int Intelligence;
-        int Charisma;
-        int Wisdom;
-        int Level;
 };
 class ClassChoice {
     public:
--- Adventurer.h
+++ Adventurer.h
@@ -4,3 +4,104 @@
 
 using namespace std;
 
+class Adventurer {
+    public:
+        Adventurer(string newname);
+        int getDexterity() {
+            return this->Dexterity;
+        }
+        void setStrength(int _Strength) {
+            this->Strength = _Strength;
+        }
+        int getStrength() {
+            return this->Strength;
+        }
+        void setConstitution(int _Constitution) {
+            this->Constitution = _Constitution;
+        }
+        int getConstitution() {
+            return this->Constitution;
+        }
+        void setIntelligence(int _Intelligence) {
+            this->Intelligence = _Intelligence;
+        }
+        int getIntelligence() {
+            return this->Intelligence;
+        }
+        void setCharisma(int _Charisma) {
+            this->Charisma = _Charisma;
+        }
+        int getCharisma() {
+            return this->Charisma;
+        }
+        void setWisdom(int _Wisdom) {
+            this->Wisdom = _Wisdom;
+        }
+        
+        void addToWisdom(int points) {
+            this->Wisdom += points;
+        }
+
+        void addToStrength(int points) {
+            this->Strength += points;
+        }
+
+        void addToIntelligence(int points) {
+            this->Intelligence += points;
+        }
+
+        void addToDexterity(int points) {
+            this->Dexterity += points;
+        }
+
+        void addToConstitution(int points) {
+            this->Constitution += points;
+        }
+
+        void addToCharisma(int points) {
+            this->Charisma += points;
+        }
+
+        int getWisdom() {
+            return this->Wisdom;
+        }
+        string getName() {
+            return this->Name;
+        }
+        void setName(string _Name) {
+            this->Name = _Name;
+        }
+        int getLevel() {
+            return this->Level;
+        }
+        int getRandomNumber(int min, int max) {
+            random_device rd;
+            mt19937 gen(rd());
+            uniform_int_distribution<int> dis(min, max);
+            return dis(gen);
+        }
+        void levelUp() {
+            this->Level=this->Level+1;
+        }
+                    
+        void display() {
+            cout << "Name: " << this->getName() << endl;
+            cout << "Dexterity: " << this->getDexterity() << endl;
+            cout << "Strength: " << this->getStrength() << endl;
+            cout << "Constitution: " << this->getConstitution() << endl;
+            cout << "Intelligence: " << this->getIntelligence() << endl;
+            cout << "Charisma: " << this->getCharisma() << endl;
+            cout << "Wisdom: " << this->getWisdom() << endl;
+            cout << "Level " << this->getLevel() << endl;
+        }
+    private:
+        string Name;
+        int Dexterity;
+        int Strength;
+        int Constitution;
+        int Intelligence;
+        int Charisma;
+        int Wisdom;
+        int Level;
+
+};
